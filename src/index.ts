910df9dda690a40f6091925ccae497366ce47ba6
Blixza <karistiara438@gmail.com>
2025-06-13 21:05:26 +0500

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
 #!/usr/bin/env node
 
-const hello = () => {};
+import { Command } from 'commander';
+
+// Declare the program
+
+const program = new Command();
+
+// Add actions onto that CLI
+
+program
+  .argument('<string>', 'string to log')
+  .option(
+    '-c, --capitalize',
+    'Capitalize the message'
+  )
+  .action(
+    (
+      message: string,
+      opts: {
+        capitalize?: boolean;
+      }
+    ) => {
+      if (opts.capitalize) {
+        console.log(message.toUpperCase());
+      } else {
+        console.log(message);
+      }
+    }
+  )
+  .description('Say hello');
+
+program
+  .command('add <numbers...>')
+  .action((numbers: number[]) => {
+    const total = numbers.reduce(
+      (a, b) => a + b,
+      0
+    );
+    console.log(`Total: ${total}`);
+  })
+  .description('Add numbers and log the total');
+
+program
+  .command('get-max-number <numbers...>')
+  .action((numbers: number[]) => {
+    const max = Math.max(...numbers);
+    console.log(`Max: ${max}`);
+  })
+  .description('Add numbers and log the total');
+
+// Execute the CLI with the given arguments
+
+program.parse(process.argv);
